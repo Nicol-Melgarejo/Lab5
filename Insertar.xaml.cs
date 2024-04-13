2fdf5610bfb485f0cc6e93c0ad250b20281bbec8
Nicol-Melgarejo <146491477+Nicol-Melgarejo@users.noreply.github.com>
2024-04-13 16:27:22 -0500

    Add files via upload

---
+++ Insertar.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace LAB05
+{
+    public partial class Insertar : Window
+    {
+        private string connectionString;
+
+        public Insertar()
+        {
+            InitializeComponent();
+            connectionString = "Data Source=LAB1504-24\\SQLEXPRESS;Initial Catalog=Nepunobd; User Id=User01;Password=123456";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+            string idEmpleado = txtIdEmpleado.Text;
+            string apellidos = txtApellidos.Text;
+            string nombre = txtNombre.Text;
+            string cargo = txtCargo.Text;
+            string tratamiento = txtTratamiento.Text;
+            DateTime fechaNacimiento = dpFechaNacimiento.SelectedDate ?? DateTime.MinValue;
+            DateTime fechaContratacion = dpFechaContratacion.SelectedDate ?? DateTime.MinValue;
+            string direccion = txtDireccion.Text;
+            string ciudad = txtCiudad.Text;
+            string region = txtRegion.Text;
+            string codPostal = txtCodPostal.Text;
+            string pais = txtPais.Text;
+            string telDomicilio = txtTelDomicilio.Text;
+            string extension = txtExtension.Text;
+            string notas = txtNotas.Text;
+
+            string query = @"INSERT INTO Empleados (IdEmpleado, Apellidos, Nombre, Cargo, Tratamiento, FechaNacimiento, FechaContratacion, Direccion, Ciudad, Region, CodPostal, Pais, TelDomicilio, Extension, Notas)
+                             VALUES (@IdEmpleado, @Apellidos, @Nombre, @Cargo, @Tratamiento, @FechaNacimiento, @FechaContratacion, @Direccion, @Ciudad, @Region, @CodPostal, @Pais, @TelDomicilio, @Extension, @Notas)";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        
+                        command.Parameters.AddWithValue("@IdEmpleado", idEmpleado);
+                        command.Parameters.AddWithValue("@Apellidos", apellidos);
+                        command.Parameters.AddWithValue("@Nombre", nombre);
+                        command.Parameters.AddWithValue("@Cargo", cargo);
+                        command.Parameters.AddWithValue("@Tratamiento", tratamiento);
+                        command.Parameters.AddWithValue("@FechaNacimiento", fechaNacimiento);
+                        command.Parameters.AddWithValue("@FechaContratacion", fechaContratacion);
+                        command.Parameters.AddWithValue("@Direccion", direccion);
+                        command.Parameters.AddWithValue("@Ciudad", ciudad);
+                        command.Parameters.AddWithValue("@Region", region);
+                        command.Parameters.AddWithValue("@CodPostal", codPostal);
+                        command.Parameters.AddWithValue("@Pais", pais);
+                        command.Parameters.AddWithValue("@TelDomicilio", telDomicilio);
+                        command.Parameters.AddWithValue("@Extension", extension);
+                        command.Parameters.AddWithValue("@Notas", notas);
+
+                        
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Empleado insertado correctamente.");
+                            LimpiarCampos();
+                        }
+                        else
+                        {
+                            MessageBox.Show("La inserción del empleado ha fallado.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar el empleado: " + ex.Message);
+            }
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Window ventanaActual = Window.GetWindow(this);
+            ventanaActual.Close();
+        }
+        private void LimpiarCampos()
+        {
+           
+            txtIdEmpleado.Text = "";
+            txtApellidos.Text = "";
+            txtNombre.Text = "";
+            txtCargo.Text = "";
+            txtTratamiento.Text = "";
+            dpFechaNacimiento.SelectedDate = null;
+            dpFechaContratacion.SelectedDate = null;
+            txtDireccion.Text = "";
+            txtCiudad.Text = "";
+            txtRegion.Text = "";
+            txtCodPostal.Text = "";
+            txtPais.Text = "";
+            txtTelDomicilio.Text = "";
+            txtExtension.Text = "";
+            txtNotas.Text = "";
+        }
+    }
+}
---
+++ Listar.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LAB05
+{
+    /// <summary>
+    /// Lógica de interacción para Listar.xaml
+    /// </summary>
+    public partial class Listar : Window
+    {
+        public Listar()
+        {
+            InitializeComponent();
+            string connectionString = "Data Source=LAB1504-24\\SQLEXPRESS;Initial Catalog=Nepunobd; User Id=User01;Password=123456";
+
+            List<Lista> empleados = new List<Lista>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("USP_ListarEmpleados", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int IdEmpleado = reader.GetInt32(reader.GetOrdinal("IdEmpleado"));
+                                string Apellidos = reader.GetString(reader.GetOrdinal("Apellidos"));
+                                string Nombre = reader.GetString(reader.GetOrdinal("Nombre"));
+                                string cargo = reader.GetString(reader.GetOrdinal("cargo"));
+
+                                empleados.Add(new Lista { IdEmpleado = IdEmpleado, Apellidos = Apellidos, Nombre = Nombre, cargo = cargo });
+                            }
+                        }
+                    }
+                }
+
+                dataGridEmpleados.ItemsSource = empleados;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        public class Lista
+        { 
+            public int IdEmpleado { get; set; }
+            public string Apellidos { get; set; }
+            public string Nombre { get; set; }
+            public string cargo { get; set; }
+
+    }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window ventanaActual = Window.GetWindow(this);
+            ventanaActual.Close();
+        }
+    }
+}
